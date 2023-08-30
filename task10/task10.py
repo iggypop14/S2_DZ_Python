69d3562d35826fd4e1290a045995dfa0834de3bb
Igor <test@mail.ru>
2023-08-30 13:11:59 +0300

    Solve task10

--- task10/task10.py
+++ task10/task10.py
@@ -19,13 +19,3 @@ if a > b:
     print(f'Нужно перевернуть {b} монеток')
 else:
     print(f'Нужно перевернуть {a} монеток')
-# или
-list_coins = []
-for i in range(n):
-    list_coins.append(randint(0, 1))
-print(list_coins)
-zero = list_coins.count(0)
-if len(list_coins) - zero < zero:
-    print(f'Нужно перевернуть {len(list_coins) - zero} монеток')
-else:
-    print(f'Нужно перевернуть {zero} монеток')
